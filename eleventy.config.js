dc43419889fe5b4e62d440611c2c1e989143d29b
cgray24 <cgray24@gmail.com>
2023-03-05 00:49:15 -0600

    adding faq to data

--- eleventy.config.js
+++ eleventy.config.js
@@ -8,7 +8,6 @@ const pluginNavigation = require("@11ty/eleventy-navigation");
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
 
-
 module.exports = function(eleventyConfig) {
 	// Copy the contents of the `public` folder to the output folder
 	// For example, `./public/css/` ends up in `_site/css/`
